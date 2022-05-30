6e13f34e0bb99803eb2d36c572e00e79932e6974
Маргарита <li13_08@mail.ru>
2022-05-30 22:34:42 +0300

    Финальная правка файла

--- gulpfile.js
+++ gulpfile.js
@@ -116,7 +116,7 @@ export const clean = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
